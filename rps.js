440686db182aeba014074760a52bb943718faaa8
Roisin-13 <86969519+Roisin-13@users.noreply.github.com>
2021-07-22 16:17:56 +0100

    curser changed and choices added to html

--- rps.js
+++ rps.js
@@ -1,39 +1,41 @@
 let rock = document.getElementById("userRock");
 let paper = document.getElementById("userPaper");
 let scissors = document.getElementById("userScissors");
-//let compChoiceDec = document.querySelector(".choices");
-//let title = document.getElementById("titleClick");
 let compChoiceIcon = document.getElementById("computerChoice");
 let userChoiceDec = document.querySelector(".myChoice")
-let iArray = ["r", "p", "s"]
+let compChoiceDec = document.querySelector(".compChoiceWords")
 let result = document.getElementById("result");
-
+let iArray = ["r", "p", "s"]
 
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
  if (randomNumber === 0) {
       compChoiceIcon.innerHTML = '<i class="far fa-hand-rock"></i>';
+      compChoiceDec.innerHTML = "Rock";
       return iArray[0];
   } else if (randomNumber === 1) {
       compChoiceIcon.innerHTML = '<i class="far fa-hand-paper"></i>';
+      compChoiceDec.innerHTML = "Paper";
       return iArray[1];
   } else if (randomNumber === 2) {
       compChoiceIcon.innerHTML = '<i class="far fa-hand-scissors"></i>';
+      compChoiceDec.innerHTML = "Scissors";
       return iArray[2];
   } 
 };
 
-
-
 function getUserChoice() {
   rock.addEventListener("click", function() {
       game(iArray[0]);
+      userChoiceDec.innerHTML = "Rock";
   });
   paper.addEventListener("click", function() {
       game(iArray[1]);
+      userChoiceDec.innerHTML = "Paper";
   });
   scissors.addEventListener("click", function() {
       game(iArray[2]);
+      userChoiceDec.innerHTML = "Scissors";
   });
   };
   getUserChoice();
